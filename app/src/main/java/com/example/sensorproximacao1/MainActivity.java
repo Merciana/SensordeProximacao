d1d835e669bba3258c9e50fe40356f843a538d70
merci <merciana.goncalves@gmail.com>
2021-04-16 17:14:52 -0300

    Sem comentário! Só fazer o teste de proximidade no emulador virtual, no real não consegui fazer o teste, mas de inicio está correto!

---
+++ app/src/main/java/com/example/sensorproximacao1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.sensorproximacao1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private SensorEventListener promitySensorListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if(proximitySensor == null){
+            Toast.makeText(this, "Sensor de proximidade não está disponível", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        promitySensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+               if(sensorEvent.values[0] < proximitySensor.getMaximumRange()){
+                   getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+               }else {
+                   getWindow().getDecorView().setBackgroundColor(Color.RED);
+               }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        sensorManager.registerListener(promitySensorListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(promitySensorListener);
+    }
+}
